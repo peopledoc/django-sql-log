c21b51d7faab555dbda01cc1d901c66407dee08b
Bruno Bord <bruno.bord@novapost.fr>
2014-12-05 15:33:37 +0100

    checking for call order (ugly)

--- demo/django_sql_log_demo/tests.py
+++ demo/django_sql_log_demo/tests.py
@@ -1,21 +1,40 @@
 from django.test import TestCase
-from mock import Mock, patch
+from mock import Mock, patch, MagicMock
 
 from django_sql_log.middleware import RequestLoggingMiddleware
 from django_sql_log.middleware import ResponseLoggingMiddleware
 RequestLoggingMiddleware = Mock(RequestLoggingMiddleware)
 ResponseLoggingMiddleware = Mock(ResponseLoggingMiddleware)
 
+REQUEST_MODULE = 'django_sql_log.middleware.RequestLoggingMiddleware' \
+                 '.process_request'
+
+RESPONSE_MODULE = 'django_sql_log.middleware.ResponseLoggingMiddleware' \
+                  '.process_response'
+
 
 class TestSettings(TestCase):
     "Test if the demo settings are correctly done"
 
     def test_request(self):
-        with patch('django_sql_log.middleware.RequestLoggingMiddleware.process_request') as mocked_request:
+        with patch(REQUEST_MODULE) as mocked_request:
             self.client.get('/')
             self.assertTrue(mocked_request.called)
 
     def test_response(self):
-        with patch('django_sql_log.middleware.ResponseLoggingMiddleware.process_response') as mocked_response:
+        with patch(RESPONSE_MODULE) as mocked_response:
             self.client.get('/')
             self.assertTrue(mocked_response.called)
+
+    def test_order(self):
+        manager = MagicMock()
+        with patch(REQUEST_MODULE) as Request:
+            with patch(RESPONSE_MODULE) as Response:
+                manager.attach_mock(Request, 'Request')
+                manager.attach_mock(Response, 'Response')
+                self.client.get('/')
+                # Ugly method, but couldn't find a better one
+                first = manager.mock_calls[0].__str__()
+                last = manager.mock_calls[-1].__str__()
+                self.assertTrue(first.startswith('call.Request'))
+                self.assertTrue(last.startswith('call.Response'))
